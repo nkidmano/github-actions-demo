8d6b13fcb8aaa700a59aa690088d81a0648adfd9
Duong Thanh Tan <nobudy55@gmail.com>
2025-03-23 05:08:22 +0700

    add flagsmith integration and feature flag handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import flagsmith from "@/utils/flagsmith";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags()
+
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
       </div>
+      <br/>
+      {flags.isFeatureEnabled('search') && <input type="search" placeholder="Search" />}
       <Link href="/about">About</Link>
     </main>
   );
